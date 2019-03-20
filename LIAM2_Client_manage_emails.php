bb160dd21edae687937d532963dcccbca6af750a
Boban Ilievski <bilievski@ssidecisions.com>
2019-03-20 10:50:39 +0100

    Path fix

--- LIAM2_Client_manage_emails.php
+++ LIAM2_Client_manage_emails.php
@@ -1,7 +1,7 @@
 <?php
 require_once(__DIR__ . '/inc/LIAM2_Client_header.inc.php');
 if (!isset($_SESSION['user_id'])) {
-    header("Location: LIAM2_Client_login.php");
+    header("Location: /LIAM2_Client_login.php");
     exit();
 } else {
     if (isset($_POST['liam2_add_another_email'])) {
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once(__DIR__ . '/inc/LIAM2_Client_header.inc.php');
 if (!isset($_SESSION['user_id'])) {
-    header("Location: LIAM2_Client_login.php");
+    header("Location: /LIAM2_Client_login.php");
     exit();
 } else {
     $user = json_decode(api(json_encode(array(
